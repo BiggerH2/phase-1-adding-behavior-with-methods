cbbb572d1b1176657189fa4103c3fa03b09e3957
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-04-14 16:33:27 +0300

    1

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+// Your code here// Define the Cat class
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Define the Dog class
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  // Define the Bird class
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Export the classes
+  module.exports = {
+    Cat,
+    Dog,
+    Bird
+  };
+  
